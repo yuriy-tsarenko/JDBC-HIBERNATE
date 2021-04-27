e2adb39ae18a336e726195ebc72f618bcd0d63b2
diledsys <diledsys@gmil.com>
2021-04-27 19:34:13 +0300

    COMMIT-1: homework_diledsys

---
+++ src/main/java/com/cbs/jdbc_hibernate/classWork/Main.java
@@ -0,0 +1,5 @@
+package com.cbs.jdbc_hibernate.classWork;
+
+public class Main {
+
+}
---
+++ src/main/java/com/cbs/jdbc_hibernate/homework/Main.java
@@ -0,0 +1,7 @@
+package com.cbs.jdbc_hibernate.homework;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
